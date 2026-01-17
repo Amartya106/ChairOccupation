18d48afa5b134e1f5c3298d7c1ab8c5d245955de
Amartya Mishra <AmartyaMishra@live.com>
2026-01-17 16:38:53 +0530

    Adjust confidence threshold in prediction

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO("runs/detect/chair_detector_v13/weights/best.pt")
 
-results = model.predict(source="/home/amartya/Downloads/Testing-20260111T063440Z-1-001/Testing", conf=0.7, save=True)
+results = model.predict(source="/home/amartya/Downloads/Testing-20260111T063440Z-1-001/Testing", conf=0.644, save=True)
 
 for result in results:
     print(result.boxes.xyxy)
